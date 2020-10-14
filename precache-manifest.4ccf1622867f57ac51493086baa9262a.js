a4016862ad3546d8f2dea6004acd2ac67f332a37
seommin <seomm1n@naver.com>
2020-10-14 17:37:17 +0900

    Updates

---
+++ precache-manifest.4ccf1622867f57ac51493086baa9262a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9c331e25c8c39d05b06841a2729806",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "744f40384ee7ef386ff3",
+    "url": "/movie_app/static/css/main.eb9f5008.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744f40384ee7ef386ff3",
+    "url": "/movie_app/static/js/main.4a6552c8.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
